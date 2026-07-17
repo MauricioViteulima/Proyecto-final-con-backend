410b81c5ae03a1a0114129633e60358ec4a58b94
MAURICIO <20233256@aloe.ulima.edu.pe>
2026-07-17 18:37:05 -0500

    correccion final

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,9 @@ import 'dotenv/config'
 import app from './src/app.js'
 import sequelize from './src/config/database.js' 
 
-const startup = async () => {
-  await sequelize.authenticate()
-  await sequelize.sync()
-}
-
-if (process.env.VERCEL) {
-  await startup()
-} else {
-  startup()
+if (!process.env.VERCEL) {
+  sequelize.authenticate()
+    .then(() => sequelize.sync())
     .then(() => {
       const PORT = process.env.PORT || 3001
       app.listen(PORT, () => {
