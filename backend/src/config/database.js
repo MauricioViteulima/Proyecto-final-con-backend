3f2aed354385841e5edfb75c54624c9b38a94ca2
MAURICIO <20233256@aloe.ulima.edu.pe>
2026-07-17 19:49:04 -0500

    cambios en database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,14 @@
 import { Sequelize } from 'sequelize'
+import pg from 'pg'
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_zd1jpOo8ZcaC@ep-rapid-shape-ack0qh2c-pooler.sa-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require'
+const connectionString =
+  process.env.DATABASE_URL ||
+  'postgresql://neondb_owner:npg_zd1jpOo8ZcaC@ep-rapid-shape-ack0qh2c-pooler.sa-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require'
 
 const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectModule: pg,
   logging: false,
   dialectOptions: {
     ssl:
